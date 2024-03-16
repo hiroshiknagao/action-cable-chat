0d515d5d2a086af84027e8dc55a1589f4762b7f9
hiroshi nagao <hi_1205@hotmail.com>
2024-03-16 15:47:48 +0900

    add sttyling with javascript

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -4,7 +4,7 @@ import { createConsumer } from "@rails/actioncable"
 // Connects to data-controller="chatroom-subscription"
 export default class extends Controller {
   static targets = ["messages"]
-  static values = { chatroomId: Number }
+  static values = { chatroomId: Number, currentUserId: Number }
   connect() { console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
     this.channel = createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
@@ -24,7 +24,28 @@ export default class extends Controller {
   }
 
   #insertMessageAndScrollDown(data) {
+    const currentUserIsSender = this.currentUserIdValue === data.sender_id
     this.messagesTarget.insertAdjacentHTML("beforeend", data)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  #buildMesssageElement(currentUserIsSender, message) {
+    return `
+      <div class="message-row d-flex
+    ${this.#justifyClass(currentUserIsSender)}">
+        <div
+    class="${this.#userStyleClass(currentUserIsSender)}">
+      ${message}
+    </div>
+  </div>
+  `
+  }
+
+  #justifyClass(currentUserIsSender) {
+    return currentUserIsSender ? "justify-content-end" : "justify-content-start"
+  }
+
+  #userStyleClass(currentUserIsSender) {
+    return currentUserIsSender ? "sender-style" : "receiver-style"
   }
 }
